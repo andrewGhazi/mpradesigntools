a000c4f8be99371085d9c96237f5d71fcc186948
Andrew Ghazi <andghazi@gmail.com>
2018-09-17 23:14:54 -0500

    script to make the table for the readme

--- R/import_freebarcodes.R
+++ R/import_freebarcodes.R
@@ -21,3 +21,22 @@ read_bc_file_write_rdata = function(bc_file) {
 
 map(bc_files,
     read_bc_file_write_rdata)
+
+#### Make the table for the readme
+
+count_bc_file_lines = function(bc_file) {
+  system(paste0('wc -l ',
+                bc_file),
+         intern = TRUE) %>%
+    stringr::str_extract('^[0-9]+') %>%
+    as.numeric
+}
+
+data_frame(barcode_set = map_chr(bc_files,
+                                 ~stringr::str_extract(.x, 'barcodes[0-9]+-[0-9]')),
+           n_barcodes = map_dbl(bc_files,
+                                count_bc_file_lines)) %>%
+  add_case(barcode_set = 'twelvemers',
+           n_barcodes = 1140292) %>%
+  arrange(barcode_set) %>%
+  knitr::kable()
